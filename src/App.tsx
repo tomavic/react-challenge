a7fc4277094241aafe5ff094b82ab2b44fb47160
tomavic <hbasheer@live.com>
2022-05-08 01:02:05 +0200

    ✨ feat: add dynamic chart data

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Select from "./components/Select";
 import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js'
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, ChartOptions } from "chart.js";
+import React from "react";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export interface Data {
-  "id": string,
-  "month": string,
-  "camp": string,
-  "country": string,
-  "school": string,
-  "lessons": number
-	
+  id: string;
+  month: string;
+  camp: string;
+  country: string;
+  school: string;
+  lessons: number;
 }
 
 function App() {
-
   const title = "Analysis Chart";
 
   const [loading, setLoading] = useState(true);
@@ -46,7 +29,7 @@ function App() {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("../db/data.json")
+      .get("https://raw.githubusercontent.com/abdelrhman-arnos/analysis-fe-challenge/master/data.json")
       .then((res) => setAnalysisData(res.data))
       .catch((err) => console.log(err));
 
@@ -57,52 +40,98 @@ function App() {
   }, []);
 
   const countries = [...new Set<any>(analysisData.map((option) => option.country))];
-  const camps = [...new Set(analysisData.map((option) => option.camp))];
-  const schools = [...new Set(analysisData.map((option) => option.school))];
+  const camps = [...new Set(analysisData.filter((option) => selectedCountry === option.country).map((option) => option.camp))];
+  const schools = [...new Set(analysisData.filter((option) => selectedCamp === option.camp).map((option) => option.school))];
 
   const handleCountrySelect = (e: React.ChangeEvent<HTMLSelectElement>) => setCountry(e.target.value);
   const handleCampSelect = (e: React.ChangeEvent<HTMLSelectElement>) => setCamp(e.target.value);
-  const handleSchoolSelect = (e: React.ChangeEvent<HTMLSelectElement>) => setSchool(e.target.value);
+  const handleSchoolSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSchool(e.target.value);
+  };
 
+  const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-  const labels = [...new Set(analysisData.map((option) => option.month))];
+  const datasets = [
+    {
+      label: selectedSchool,
+      data: analysisData.filter((option) => selectedSchool === option.school).map((option) => option.lessons),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      pointStyle: "circle",
+      pointRadius: 8,
+      pointHoverRadius: 12,
+    },
+  ];
+
+  const options: ChartOptions<any> = {
+    pointBackgroundColor: "#fff",
+    responsive: true,
+    plugins: {
+      legend: {
+        title: {
+          position: "start",
+        },
+        position: "right",
+        labels: {
+          usePointStyle: true,
+        },
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          color: "rgba(255, 99, 132, 0.0)",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "No. of Lessons",
+        },
+        grid: {
+          drawBorder: false,
+          color: "#000000",
+        },
+      },
+    },
+  };
 
   const data = {
     labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: analysisData.map((item) => item.lessons),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        pointStyle: 'circle',
-        pointRadius: 10,
-        pointHoverRadius: 15
-      },
-    ],
+    datasets,
   };
 
   return (
     <div className="App">
       <h1 className="m-3">{title}</h1>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <Select id="country" label="Select Country" loading={loading} options={countries} selectedValue={selectedCountry} handleOnSelect={handleCountrySelect} />
+      {loading ? (
+        <img src={process.env.PUBLIC_URL + '/assets/loader.gif'} alt="loader" style={{ margin: "0 auto", display: "block", width: '300px' }} />
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <Select id="country" label="Select Country" loading={loading} options={countries} selectedValue={selectedCountry} handleOnSelect={handleCountrySelect} />
+            </div>
+            <div className="col-4">
+              <Select id="camp" label="Select Camp" loading={loading} options={camps} selectedValue={selectedCamp} handleOnSelect={handleCampSelect} />
+            </div>
+            <div className="col-4">
+              <Select id="school" label="Select School" loading={loading} options={schools} selectedValue={selectedSchool} handleOnSelect={handleSchoolSelect} />
+            </div>
           </div>
-          <div className="col-4">
-            <Select id="camp" label="Select Camp" loading={loading} options={camps} selectedValue={selectedCamp} handleOnSelect={handleCampSelect} />
-          </div>
-          <div className="col-4">
-            <Select id="school" label="Select School" loading={loading} options={schools} selectedValue={selectedSchool} handleOnSelect={handleSchoolSelect} />
-          </div>
-        </div>
 
-        <div className="row">
-          <Line data={data} />
+          <div className="row my-4">
+            {selectedSchool ? (
+              <Line data={data} options={options} />
+            ) : (
+              <p className="alert alert-danger" role="alert">
+                Please select to view Analytics
+              </p>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
